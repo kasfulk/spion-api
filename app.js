b982b89d8e37b791e0cf65618e9b91421d0ef46a
Kasjful Kurniawan <kasful.bpp@gmail.com>
2022-01-20 15:42:43 +0800

    remove auth /

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import cors from 'cors';
 import appRoute from './src/routes/index.js';
 import noAuthRoute from './src/routes/route-no-auth.js';
 import dotenv from 'dotenv';
-import { tokenCheck } from './src/helpers/auth.js';
 
 dotenv.config({ path: '.env' });
 const app = express();
@@ -12,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/no-auth', noAuthRoute);
-app.use('/', tokenCheck, appRoute);
+app.use('/', appRoute);
 
 const port = process.env.PORT || 3000;
 
